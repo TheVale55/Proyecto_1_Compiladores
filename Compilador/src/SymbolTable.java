9f9bae635e52a9967d7e1f221adfe0a0533adccf
JoshJD11 <josh.temporal11@gmail.com>
2025-01-22 17:01:14 -0600

    validacionsemanticaenoperaciones

--- Compilador/src/SymbolTable.java
+++ Compilador/src/SymbolTable.java
@@ -2,8 +2,17 @@ import java.util.*;
 
 public class SymbolTable {
 
-    private Stack<HashMap<String, String>> scopes;
     public static final int TYPE_INDEX = 0;
+    public static final String SEPARATOR = ":";
+    public static final List<String> DATA_TYPES = Arrays.asList(
+        "int", "float", "char", "bool", "string"
+    );
+    public static final int FLOAT_TYPE_INDEX = 1;
+    public static final int STRING_TYPE_INDEX = 4;
+    public static final String EMPTY_STRING = "";
+    public static final String MODULE_OPERATOR = "%";
+
+    private Stack<HashMap<String, String>> scopes;
 
 
     public SymbolTable() {
@@ -33,33 +42,44 @@ public class SymbolTable {
     }
 
 
-    public String getType(String key) {
-        for(int i=0; i<scopes.size(); i++) {
-            if(scopes.get(i).containsKey(key)) {
-                String[] info = scopes.get(i).get(key).split(":");
-                return info[TYPE_INDEX]; //cambiar indice en el que se pone el tipo
-            }
-        }
-        return "Null"; //No existe el valor en ningun scope
+    private boolean isDataType(String data) {
+        return DATA_TYPES.contains(data);
     }
 
 
-    public boolean verifyType(String type, String data) {
-        String [] values = data.split(":");
-        for(int i=0; i<values.length; i++) {
-            if(!(values[i].equals(type) || getType(values[i]).equals(type))) {
-                return false;
+    public String getType(String key) {
+        if(isDataType(key)) {
+            return key;
+        }
+        for(int i=0; i<scopes.size(); i++) {
+            if(scopes.get(i).containsKey(key)) {
+                String[] info = scopes.get(i).get(key).split(SEPARATOR);
+                return info[TYPE_INDEX]; //cambiar indice en el que se pone el tipo
             }
         }
-        return true;
+        return EMPTY_STRING; //No existe el tipo en ningun scope
+    }
+
+
+    public boolean verifyType(String varType, String exprFinalType) {
+        return varType.equals(exprFinalType);
     } 
 
+
+
+    public boolean validateOperation(String rightOperand, String operator, String leftOperand) {
+        String rightOperandType = getType(rightOperand);
+        String leftOperandType = getType(leftOperand);
+        return !(rightOperandType.equals(DATA_TYPES.get(STRING_TYPE_INDEX)) || leftOperandType.equals(DATA_TYPES.get(STRING_TYPE_INDEX)) || 
+        ((rightOperandType.equals(DATA_TYPES.get(FLOAT_TYPE_INDEX)) || leftOperandType.equals(DATA_TYPES.get(FLOAT_TYPE_INDEX))) && operator.equals(MODULE_OPERATOR))) && 
+        rightOperandType.equals(leftOperandType);
+    }
 
 
     public void printScope() {
         if(!scopes.empty()) {
             System.out.println(scopes.peek());
-            System.out.println("");
+            System.out.println(EMPTY_STRING);
         }
     }
 
