6363a20629b35268ebfe8dadf8324226407c9752
JoshJD11 <josh.temporal11@gmail.com>
2025-01-31 01:15:10 -0600

    avance_mips

--- Compilador/src/MIPSGenerator.java
+++ Compilador/src/MIPSGenerator.java
@@ -1,19 +1,108 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
 
 
 public class MIPSGenerator {
 
+    public static final int IF_COUNTER_INDEX = 0;
+    public static final int ELSE_COUNTER_INDEX = 1;
+    public static final int WHILE_COUNTER_INDEX = 2;
+    public static final int FOR_COUNTER_INDEX = 3;
+    public static final int SWITCH_COUNTER_INDEX = 4;
+    public static final int CASE_COUNTER_INDEX = 5;
+    public static final int DEFAULT_COUNTER_INDEX = 6;
+    public static final int STRUCT_TYPE = 0;
+    public static final int STRUCT_NUMBER = 1;
+
+
     private String fileName;
     private StringBuilder textSection;
     private StringBuilder dataSection;
+    private List<Integer> structCounter = Arrays.asList(
+        0, 0, 0, 0, 0, 0, 0
+    );
+    private Stack<String> structController; //--> Como los scopes, saber la estructura en la que voy para saber dónde saltar.
+    private HashMap<String,String> registerMap; //sin usar aún --> para saber que variable quedó en qué registro.
+    private HashMap<String, Stack<String>> registerHandler; //sin usar aún --> saber que registros están disponibles.
+
 
 
     public MIPSGenerator(String fileName) {
         this.fileName = fileName;
         this.textSection = new StringBuilder();
         this.dataSection = new StringBuilder();
+        this.structController = new Stack<String>();
+        this.registerMap = new HashMap<String,String>();
+        this.registerHandler = new HashMap<String, Stack<String>>();
+
+
+
     }
+
+
+
+    private int getStructCountIndex(String structName) {
+        switch (structName) {
+            case "while":
+                return WHILE_COUNTER_INDEX;
+        
+            case "if":
+                return IF_COUNTER_INDEX;
+
+            case "for":
+                return FOR_COUNTER_INDEX;
+
+            case "else":
+                return ELSE_COUNTER_INDEX;
+
+            case "switch":
+                return SWITCH_COUNTER_INDEX;
+
+            case "case":
+                return CASE_COUNTER_INDEX;
+
+            case "default":
+                return DEFAULT_COUNTER_INDEX;
+        }
+        return -1;
+    }
+
+
+
+    public void createStartTag(String structName) {
+        int structIndex = getStructCountIndex(structName);
+        int structCount = this.structCounter.get(structIndex);
+        this.textSection.append(structName + "Start" + structCount + ":\n");
+        this.structController.add(structName + ":" + structCount);
+        this.structCounter.set(structIndex, structCount + 1);
+    }
+
+
+
+    public void createEndTag() {
+        if(!structController.empty()) {
+            String[] data = this.structController.peek().split(":");
+            this.textSection.append(data[STRUCT_TYPE] + "END" + data[STRUCT_NUMBER] + ":\n");
+            this.structController.pop();
+        }
+
+    }
+
+
+
+    public void breakLastLoopCreated() {
+        for(int i = structController.size()-1; i>=0; i--) {
+            String[] data = this.structController.get(i).split(":");
+            if(data[STRUCT_TYPE].equals("switch") || data[STRUCT_TYPE].equals("for") || data[STRUCT_TYPE].equals("while")) {
+                this.textSection.append("j " + data[STRUCT_TYPE] + "END" + data[STRUCT_NUMBER] + "\n");
+            }
+        }
+    }
+
 
 
     public void exitProgram() {
@@ -21,6 +110,7 @@ public class MIPSGenerator {
     }
 
 
+    
     public void printInt(int value) {
         this.textSection.append("li $v0, 1\nli $a0, " + value + "\nsyscall\n");
     }
